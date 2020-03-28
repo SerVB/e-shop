a4dbd1140978aa4e13813753532e51e1267ebf8d
SerVB <servbul@yandex.ru>
2020-03-28 23:18:05 +0300

    Fix serialization to {} instead of {"data": null}

--- server/src/main/kotlin/io/github/servb/eShop/util/Response.kt
+++ server/src/main/kotlin/io/github/servb/eShop/util/Response.kt
@@ -1,5 +1,11 @@
 package io.github.servb.eShop.util
 
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
+
+
 interface SuccessResult {
 
     val ok: Boolean
@@ -14,8 +20,17 @@ interface OptionalResult<out ResultType : Any> {
 
     val data: ResultType?
 
+    @JsonSerialize(using = FAIL.FailSerializer::class)
     object FAIL : OptionalResult<Nothing> {
 
         override val data = null
+
+        class FailSerializer @JvmOverloads constructor(t: Class<FAIL>? = null) : StdSerializer<FAIL>(t) {
+            override fun serialize(value: FAIL, generator: JsonGenerator, provider: SerializerProvider) {
+                generator.writeStartObject()
+                generator.writeObjectField("data", null)
+                generator.writeEndObject()
+            }
+        }
     }
 }
